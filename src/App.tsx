92811735b12c39338ec20b95a018b358bd133114
BaHGet <abosama150@gmail.com>
2024-07-15 13:17:30 +0300

    initialize  tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    'hi'
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello, Tailwind CSS with React and TypeScript!
+      </h1>
+    </div>
   )
 }
 
